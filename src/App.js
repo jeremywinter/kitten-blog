039f161a50245aae5cee473cd681f148b6e8d413
Jeremy Winter <jeremywinter@users.noreply.github.com>
2022-04-05 19:53:41 -0500

    Round1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const link = "http://google.com";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>likes { likes } times</p>
+        <p> { 10 } </p>
+        <p> { Math.random() * 10 } </p>
+        <a href={link}>google site</a>
+
+      </div>
     </div>
   );
 }
