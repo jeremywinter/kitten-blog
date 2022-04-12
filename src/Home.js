f8c1fae211981ed66be31e6a82f2333c54a8d127
Jeremy Winter <jeremywinter@users.noreply.github.com>
2022-04-11 19:54:46 -0500

    round11

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-	const [blogs, setBlogs] = useState([
-		{ title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-		{ title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-		{ title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-	]);
+	const [blogs, setBlogs] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8001/blogs')
+    // npx json-server --watch data/db.json --port 8001
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      setBlogs(data);
+    });   
+  }, []);
 
 	return (  
 		<div className="home">
-			<BlogList blogs={blogs} title="All blogs!"/>
+			{blogs && <BlogList blogs={blogs} title="All blogs!" />}
 		</div>
 	);
 }
